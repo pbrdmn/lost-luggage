cfe53a78c91430d7efb8a22994707b80288c4302
Philip Boardman <github@brd.mn>
2025-02-11 14:29:33 +1100

    Describing some methods

---
+++ methods/describe_item.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Load item from city
+# // if no item found return nothing
+# // if item has been found return description
+# "{name} - {description}"
+# // if item has not been found return lost luggage message
+# "A piece of lost luggage"
---
+++ methods/show_flights.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Load flights from the current city
+# Display flights destination, duration, and cost
+# Option to book a flight, view departures from another city, or close
+# Wait for user input
